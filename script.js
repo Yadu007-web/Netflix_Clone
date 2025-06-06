9d506a601ab5ea417b9bce02cc1c9c1f0787c2c6
Yadu007-web <yadunandant4@gmail.com>
2025-06-07 01:08:50 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+const profileIcon = document.querySelector(".profile-icon");
+profileIcon.addEventListener("click", () => {
+  window.location.href = "profile.html";
+});
